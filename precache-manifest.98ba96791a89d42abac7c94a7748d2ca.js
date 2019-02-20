ca08913d192ae42274313379378312be15d36d96
nasyaoris <nasyaoris@gmail.com>
2019-02-20 19:50:16 +0700

    yeyyy

---
+++ precache-manifest.98ba96791a89d42abac7c94a7748d2ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "43834cb97e44e4e88d06d8563592a6d4",
+    "url": "/static/media/nasya.43834cb9.jpg"
+  },
+  {
+    "revision": "291f2b4f391ea5437e9798d91a6c40ee",
+    "url": "/static/media/S__2637981.291f2b4f.jpg"
+  },
+  {
+    "revision": "d9b348a47010f88b2bf99e9a5f932a7c",
+    "url": "/static/media/S__1785859.d9b348a4.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "52da0081a1d62799ebed",
+    "url": "/static/js/main.52da0081.chunk.js"
+  },
+  {
+    "revision": "4c3279a00f5af1af9717",
+    "url": "/static/js/1.4c3279a0.chunk.js"
+  },
+  {
+    "revision": "52da0081a1d62799ebed",
+    "url": "/static/css/main.a7d2e46a.chunk.css"
+  },
+  {
+    "revision": "183ffc835f67181e02baad678fd46c75",
+    "url": "/index.html"
+  }
+];
